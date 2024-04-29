bd64c04fb5ca385d51a5b2a066bffceddff36b16
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-04-29 18:02:51 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import userSlice from './Redux/userSlice';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={userSlice}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
