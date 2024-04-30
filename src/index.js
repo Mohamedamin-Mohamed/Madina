43d6ef29faf21317dcdd3af5d58d495cd07d27e5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-04-30 06:56:47 -0500

    Provider component connected to the store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import userSlice from './Redux/userSlice';
+import store from './Redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={userSlice}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
