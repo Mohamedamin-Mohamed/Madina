24ead67fc8ff4ad09daa0b3ef1a1ce3fcbd8cd6e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-04-30 11:18:15 -0500

    custom amount addition

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,45 @@
-import { FaApple } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import CreditCardPay from './CreditCardPay';
+import { FaApple } from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
+import CreditCardPay from './CreditCardPay'
+import { FaDollarSign } from "react-icons/fa6"
+
 import{useSelector, useDispatch } from 'react-redux'
-import { toggleCreditCardDisplay, setAmount } from './Redux/userSlice'
+import { toggleCreditCardDisplay, setAmount, setOrderTotal } from './Redux/userSlice'
 import { useEffect, useState } from "react";
 
 const Navbar = ()=>{
     const userInfo = useSelector(state=>state.userInfo)
     const dispatch = useDispatch()
     const[color, setColor] = useState([false, false, false])
+    const[customAmount, setCustomAmount] = useState('')
    
    useEffect(()=>{
     const amountUserSelects = ()=>{
+        if(customAmount !== '') setCustomAmount('')
         if (color[0]){
             dispatch(setAmount(200))
+            dispatch(setOrderTotal(202.40))
         }
         if (color[1]){
             dispatch(setAmount(250))
+            dispatch(setOrderTotal(252.40))
+
         }
         if (color[2]){
             dispatch(setAmount(300))
+            dispatch(setOrderTotal(302.40))
         }
         }
         amountUserSelects()
+        
    }, [color])
+   const handleCustomAmount = (amount)=>{
+   
+    const parsedAmount = parseInt(amount, 10); // Parse the input value to an integer
+    setCustomAmount(isNaN(parsedAmount) ? '' : parsedAmount); // Update the custom amount state
+    dispatch(setAmount(isNaN(parsedAmount) ? 0 : parsedAmount)); // Dispatch the parsed integer amount\
+    dispatch(setOrderTotal(isNaN(parsedAmount) ? 0 : parsedAmount + 2.40)); // Dispatch the parsed integer amount with additional value
+   }
     return(
         <div className='flex justify-center items-center  h-screen'>
             <div className='flex justify-center w-[1000px]'>
@@ -47,7 +63,16 @@ const Navbar = ()=>{
             </button>
             
             </div>
-            <p>Custom Amount</p>
+            <div className="flex my-4">
+            <p className="text-lg mt-2">Custom Amount</p>
+            <div className="flex mt-1 ml-4 border rounded-md">
+                <FaDollarSign size={15} className="mt-2 ml-3"/>
+            <input className=":w-[100px] bg-gray-100 outline-none p-1  ml-4" type="number" name="customAmount" placeholder="Custom Amount" 
+            onChange={(e)=>handleCustomAmount(e.target.value)} 
+            value={customAmount} disabled={color.some((isSelected)=> isSelected)} />
+          </div>
+
+            </div>
             <div className='flex flex-col border p-14 mb-6'>
               
                 <button className='flex bg-black p-2 text-white w-[350px] justify-center hover:cursor-pointer'>
